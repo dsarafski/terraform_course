ccfb30824f51bf2e544457b41f082a9274f38d74
dimitar-sarafski-axway <dsarafski@axway.com>
2020-02-11 11:29:11 +0200

    EC2+UserData+1variable+SG

--- main.tf
+++ main.tf
@@ -3,10 +3,33 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0713f98de93617bb4"
+  ami           = "ami-035966e8adab4aaad"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-test"
   }
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance_sg"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
