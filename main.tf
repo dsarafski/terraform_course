6604bd21f1ba4d9d3298ac75e66bdd5ef9987849
dimitar-sarafski-axway <dsarafski@axway.com>
2020-02-11 10:31:27 +0200

    First commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0713f98de93617bb4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-test"
+  }
+}
